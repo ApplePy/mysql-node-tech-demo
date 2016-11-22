1c6884e9a58447adf0b1e5581f9de9b082d93f57
Darryl Murray <darrylmurray@me.com>
2016-11-22 17:24:07 -0500

    WIP: Rough draft of how post should function. UNTESTED.

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,108 @@
 var express = require('express');
 var router = express.Router();
 var db_cmds = require('db_cmds');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var getUserID = function(res) {
+    var jsoncookie = cookieParser.JSONCookie(req.cookies);
+    if (jsoncookie != undefined)
+        return jsoncookie.userid;
+    else return undefined;
+};
+
+// Redirect if logged in
+var loggedInRedirect = function(res) {
+    var userid = getUserID(res);
+    if (userid != undefined) {
+        res.redirect('/welcome');
+        return true;
+    }
+    else {
+        return false;
+    }
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var cb = function(suggestedTrack) {
+    var cb = function() {
         res.render('index', {title: 'Apollo'});
     };
+
+    // Redirect if logged in
+    if (!loggedInRedirect(res)) cb();
 });
 
-router.route('/login').get(function(req, res){
-    var cb = function(suggestedTrack) {
-        res.render('login', {title: 'Log In'});
-    };
-});
+router.route('/login')
+    .get(function (req, res) {
+        var cb = function (suggestedTrack) {
+            res.render('login', {title: 'Log In'});
+        };
 
-router.route('/create-user').get(function(req, res){
-    var cb = function(suggestedTrack) {
-        res.render('create-user', {title: 'Create User'});
-    };
-});
+        // Redirect if logged in
+        if (!loggedInRedirect(res)) cb();
+    })
+    .post(function (req, res) {
+        var username = req.body.username;
+        var password = req.body.password;
+
+        // Set cookie on success, then redirect to welcome
+        var successCallback = function(userid) {
+            res.cookie("apollo", "{userid: "+ userid +"}", {overwrite: true, maxAge: 1000 * 60 * 60 * 24}); // 24 hour expiry
+            res.redirect('/welcome');
+        };
+
+        // Handle bad credentials
+        var failureCallback = function() {/*TODO: fill in*/};
+
+        // Check credentials
+        db_cmds.loginUser(username, password, successCallback, failureCallback);
+    });
+
+router.route('/create-user')
+    .get(function (req, res) {
+        var cb = function (suggestedTrack) {
+            res.render('create-user', {title: 'Create User'});
+        };
+
+        // Redirect if logged in
+        if (!loggedInRedirect(res)) cb();
+    })
+    .post(function(req, res) {
+        var username = req.body.username;
+        var password = req.body.password;
+        var firstname = req.body.firstname;
+        var lastname = req.body.lastname;
+
+        // Set cookie on success, then redirect to welcome
+        var successCallback = function(userid) {
+            res.cookie("apollo", "{userid: "+ userid +"}", {overwrite: true, maxAge: 1000 * 60 * 60 * 24}); // 24 hour expiry
+            res.redirect('/welcome');
+        };
+
+        // Handle bad create
+        var failureCallback = function(message) {/*TODO: fill in*/};
+
+        // Check credentials
+        db_cmds.createUser(username, password, firstname, lastname, successCallback, failureCallback);
+    });
 
 router.route('/welcome').get(function(req, res){
     var cb = function(suggestedTrack) {
         res.render('welcome', {title: 'Welcome', suggestedTrack: suggestedTrack})
     };
 
-    var userid = *something*;
-
-    db_cmds.suggestedtrack(userid, cb);
+    var userid = getUserID(res);
+    db_cmds.suggestedTrack(userid, cb);
 });
 
 router.route('/settings').get(function(req, res){
     var cb = function(suggestedTrack) {
         res.render('settings', {title: 'Settings'});
     };
+
+    var userid = getUserID(res);
 });
 
 module.exports = router;
