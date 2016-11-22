551757d241236a88ac3825299b1d095fbb00ff28
dzagar <dzagar@uwo.ca>
2016-11-22 15:02:22 -0500

    Rough mockup of welcome.jade, changed port

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ db.connect(db.MODE_PRODUCTION, function(err) {
         console.log('Unable to connect to MySQL.');
         process.exit(1);
     } else {
-        app.listen(3000, function() {
-            console.log('Listening on port 3000...');
+        app.listen(8080, function() {
+            console.log('Listening on port 8080...');
         });
     }
 });
