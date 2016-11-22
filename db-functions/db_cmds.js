1c4bee09610054ab1aacb04e143389f6580cd934
Darryl Murray <darrylmurray@me.com>
2016-11-22 17:21:09 -0500

    WIP: Framework for login and create user done.

--- db-functions/db_cmds.js
+++ db-functions/db_cmds.js
@@ -2,11 +2,11 @@
  * Created by darryl on 2016-11-22.
  */
 
-var mysql = require('mysql');
+var db = require('../db');
 
-exports.suggestedtrack = function(userid, cb){
-    db.query( {
-        sql: "SELECT track.trackID, trackName, artistName, musicgroupName " +
+exports.suggestedTrack = function (userid, cb) {
+    db.query(
+        "SELECT track.trackID, trackName, artistName, musicgroupName " +
         "FROM musicgroupmembership AS gm " +
         "JOIN musicgroup AS g " +
         "ON gm.musicgroup = g.musicgroupID " +
@@ -23,6 +23,43 @@ exports.suggestedtrack = function(userid, cb){
         "WHERE gm.user != ? " +
         "ORDER BY RAND() " +
         "LIMIT 1;",
-        async: false
-    }, cb);
+        [userid],
+        cb);
+};
+
+exports.loginUser = function(username, password, successCallback, failureCallback){
+    var cbmiddle = function(whatGoesHereLOL){           // TODO: figure this out
+        if (InsertWorked) {                             // TODO: figure this out
+            var userid = "Where does this come from?";  // TODO: figure this out
+            successCallback(userid);
+        }
+        else
+            failureCallback();
+    };
+
+    db.query(
+        "SELECT userID " +
+        "FROM user " +
+        "WHERE username = ? " +
+        "AND password = ?",
+        [username, password],
+        cbmiddle);
+};
+
+exports.createUser = function(username, password, prefFirstName, lastName, successCallback, failureCallback){
+    var cbmiddle1 = function(whatGoesHereLOL){  // TODO: figure this out
+        if (InsertWorked) {                     // TODO: figure this out
+            var failure = function() {failureCallback("Undefined error.")};     // Application of arguments
+            exports.loginUser(username, password, successCallback, failure);
+        }
+        else {
+            failureCallback(message);           // TODO: figure this out
+        }
+    };
+
+    db.query(
+      "INSERT INTO tracks(username, password, prefFirstName, lastName) " +
+      "VALUES(?, ?, ?, ?)",
+      [username, password, prefFirstName, lastName],
+      cbmiddle1);
 };
