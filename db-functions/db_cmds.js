03e2b467b68f54da0196b9f9fac01156bb5eadde
Darryl Murray <darrylmurray@me.com>
2016-11-23 10:00:21 -0500

    Fixed db_cmds

--- db-functions/db_cmds.js
+++ db-functions/db_cmds.js
@@ -5,55 +5,53 @@
 var db = require('../db');
 
 exports.suggestedTrack = function (userid, cb) {
-    db.query(
-        "SELECT track.trackID, trackName, artistName, musicgroupName " +
-        "FROM musicgroupmembership AS gm " +
-        "JOIN musicgroup AS g " +
-        "ON gm.musicgroup = g.musicgroupID " +
-        "JOIN sharedplaylists AS sp " +
-        "ON g.musicgroupID = sp.musicgroup " +
-        "JOIN playlist AS p " +
-        "ON sp.playlist = p.playlistID " +
-        "JOIN playlistordering AS po " +
-        "ON p.playlistID = po.playlistID " +
-        "JOIN track " +
-        "ON po.trackID = track.trackID " +
-        "JOIN artist " +
-        "ON track.artist = artist.artistID " +
-        "WHERE gm.user != ? " +
-        "ORDER BY RAND() " +
-        "LIMIT 1;",
-        [userid],
+    db.query({
+            sql: "SELECT track.trackID, trackName, artistName, musicgroupName " +
+            "FROM musicgroupmembership AS gm " +
+            "JOIN musicgroup AS g " +
+            "ON gm.musicgroup = g.musicgroupID " +
+            "JOIN sharedplaylists AS sp " +
+            "ON g.musicgroupID = sp.musicgroup " +
+            "JOIN playlist AS p " +
+            "ON sp.playlist = p.playlistID " +
+            "JOIN playlistordering AS po " +
+            "ON p.playlistID = po.playlistID " +
+            "JOIN track " +
+            "ON po.trackID = track.trackID " +
+            "JOIN artist " +
+            "ON track.artist = artist.artistID " +
+            "WHERE gm.user != ? " +
+            "ORDER BY RAND() " +
+            "LIMIT 1;",
+            values: [userid]
+        },
         cb);
 };
 
 exports.loginUser = function(username, password, successCallback, failureCallback){
     var cbmiddle = function(error, results){           // TODO: figure this out
         if (error) {                             // TODO: figure this out
-            failureCallback(error);
-            //var userid = "Where does this come from?";  // TODO: figure this out
-            //successCallback(userid);
+            failureCallback();
         }
         else
-            console.log(results);
-            //failureCallback();
+            var userid = results[0].userID;
+            successCallback(userid);
     };
 
-    db.query(
-        "SELECT userID " +
-        "FROM user " +
-        "WHERE username = ? " +
-        "AND password = ?",
-        [username, password],
+    db.query({
+            sql: "SELECT userID " +
+            "FROM user " +
+            "WHERE username = ? " +
+            "AND password = ?",
+            values: [username, password]
+        },
         cbmiddle);
 };
 
 exports.createUser = function(username, password, prefFirstName, lastName, successCallback, failureCallback){
-    console.log('entered createUser');
-    var cbmiddle1 = function(error){  // TODO: figure this out
-        if (error) {                     // TODO: figure this out
-            //failureCallback('failed create user');           // TODO: figure this out
-            console.log('failure callback called create user')
+    var cbmiddle1 = function(error, results){
+        if (error) {
+            failureCallback('Create user failed.');
         }
         else {
             var failure = function() {failureCallback("Undefined error.")};     // Application of arguments
@@ -61,9 +59,10 @@ exports.createUser = function(username, password, prefFirstName, lastName, succe
         }
     };
 
-    db.query(
-      "INSERT INTO tracks(username, password, prefFirstName, lastName) " +
-      "VALUES(?, ?, ?, ?)",
-      [username, password, prefFirstName, lastName],
-      cbmiddle1);
+    db.query({
+            sql: "INSERT INTO tracks(username, password, prefFirstName, lastName) " +
+            "VALUES(?, ?, ?, ?)",
+            values: [username, password, prefFirstName, lastName]
+        },
+        cbmiddle1);
 };
