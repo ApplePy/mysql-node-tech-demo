6b0c070a81892a1b0a92b02b2332a601497f3bb2
dzagar <dzagar@uwo.ca>
2016-11-29 23:01:40 -0500

    Db.js commented, app.js is not being commented

--- db.js
+++ db.js
@@ -1,115 +1,61 @@
 /**
  * Created by danazagar on 2016-11-21.
  */
-var mysql = require('mysql')
-    , async = require('async');
-var dbFcns = require('./db');
+var mysql = require('mysql');
 
+/**
+ * Set database.
+ * @type {string}
+ */
 var PRODUCTION_DB = 'apollo'
-    , TEST_DB = 'v2'
+    , TEST_DB = 'v2';
 
-exports.MODE_TEST = 'mode_test'
-exports.MODE_PRODUCTION = 'mode_production'
+/**
+ * Set database mode.
+ * @type {string}
+ */
+exports.MODE_TEST = 'mode_test';
+exports.MODE_PRODUCTION = 'mode_production';
 
+/**
+ * Represent state of database.
+ * @type {{pool: null, mode: null}}
+ */
 var state = {
     pool: null,
     mode: null,
-}
+};
 
+/**
+ * Connect to database via createPool.
+ * @param mode
+ * @param done
+ */
 exports.connect = function(mode, done) {
     state.pool = mysql.createPool({
         host: 'puddleglum.murrayweb.ca',
         user: 'root',
         password: 'se3309a',
         database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
-    })
+    });
 
-    state.mode = mode
-    done()
+    state.mode = mode;
+    done();
 }
 
-// Convenience function.
+/**
+ * Convenience function to query.
+ * @param query
+ * @param cb
+ */
 exports.query = function(query, cb) {
     state.pool.query(query, cb);
 };
 
+/**
+ * Convenience function to get pool.
+ * @returns {null}
+ */
 exports.get = function() {
-    return state.pool
+    return state.pool;
 };
-
-/**
- * Convenience wrapper for database connection from pool
- */
-exports.withConnection = function (pool, body, callback) {
-    state.pool.getConnection(function(err, db) {
-        if (err) return callback(err);
-
-        body(db, finished);
-
-        function finished() {
-            db.release();
-            callback.apply(this, arguments);
-        }
-    })
-}
-
-/**
- * Convenience wrapper for database connection in a transaction
- */
-exports.inTransaction = function (pool, body, callback) {
-    dbFcns.withConnection(pool, function(db, done) {
-
-        db.beginTransaction(function(err) {
-            if (err) return done(err);
-
-            body(db, finished)
-        })
-
-        // Commit or rollback transaction, then proxy callback
-        function finished(err) {
-            var context = this;
-            var args = arguments;
-
-            if (err) {
-                if (err == 'rollback') {
-                    args[0] = err = null;
-                }
-                db.rollback(function() { done.apply(context, args) });
-            } else {
-                db.commit(function(err) {
-                    args[0] = err;
-                    done.apply(context, args)
-                })
-            }
-        }
-    }, callback)
-}
-
-
-
-
-// DONT NEED THIS
-
-exports.fixtures = function(data, done) {
-    var pool = state.pool
-    if (!pool) return done(new Error('Missing database connection.'))
-
-    var names = Object.keys(data.tables)
-    async.each(names, function(name, cb) {
-        async.each(data.tables[name], function(row, cb) {
-            var keys = Object.keys(row)
-                , values = keys.map(function(key) { return "'" + row[key] + "'" })
-
-            pool.query('INSERT INTO ' + name + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')', cb)
-        }, cb)
-    }, done)
-}
-
-exports.drop = function(tables, done) {
-    var pool = state.pool
-    if (!pool) return done(new Error('Missing database connection.'))
-
-    async.each(tables, function (name, cb) {
-        pool.query('DELETE * FROM ' + name, cb)
-    }, done)
-}
